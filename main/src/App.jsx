a22b295a7e30fdcc5cf770cd73a1b9311e985f81
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2024-01-13 20:19:29 +0530

    songs

--- main/src/App.jsx
+++ main/src/App.jsx
@@ -1,8 +1,12 @@
 import { useState,useEffect, useRef } from 'react'
 import './App.css'
 import Card from './Components/Card';
-import musicc from "./assets/music/Kalank Flute.mp3";
-import musicc1 from "./assets/music/harry.mp3";
+import musicc from "./assets/music/jeevan.mp3";
+import musicc1 from "./assets/music/kahi.mp3";
+import musicc2 from "./assets/music/sama.mp3";
+import musicc3 from "./assets/music/tere.mp3";
+import musicc4 from "./assets/music/tu.mp3";
+import musicc5 from "./assets/music/yeh.mp3";
 
 
 const App = () => {
@@ -11,17 +15,29 @@ const App = () => {
   {
     
     img_src:
-      "https://miriamruthross.files.wordpress.com/2014/09/1280x720-sos.jpg",
+      "https://i.ytimg.com/vi/idUREJ2tn54/sddefault.jpg",
     title:musicc
   },
   {
     img_src:
-      "https://miriamruthross.files.wordpress.com/2014/09/1280x720-sos.jpg",
+      "https://i.ytimg.com/vi/QZL6HlCXUNI/maxresdefault.jpg",
     title:musicc1
   },
   {
-    img_src: "https://i.ytimg.com/vi/YX8NjvkjWgw/maxresdefault.jpg",
-    title:musicc
+    img_src: "https://i.ytimg.com/vi/vDFyP6gliFQ/maxresdefault.jpg",
+    title:musicc2
+  },
+  {
+    img_src: "https://i.ytimg.com/vi/8-HnmVg0-O8/hqdefault.jpg",
+    title:musicc3
+  },
+  {
+    img_src: "https://i.ytimg.com/vi/5kwgFJZ1xvY/maxresdefault.jpg",
+    title:musicc4
+  },
+  {
+    img_src: "https://i.ytimg.com/vi/z-vcE-UTc-0/mqdefault.jpg",
+    title:musicc5
   },
 ]
   const audio = document.getElementById("audio_tag");
@@ -33,10 +49,10 @@ const App = () => {
     }
   }, [audioRef, mySound]);
   return (
-    <div className='min-h-screen flex gap-[2rem] items-center justify-center'>
+    <div className='min-h-screen flex gap-[2rem] items-center justify-center flex-wrap'>
       {songs.map((song)=>
       { return(
-        <Card path={song.title} name="Kalank"  setMySound={setMySound}  key={song.title}/>);
+        <Card path={song.title} name="Kalank"  setMySound={setMySound}  key={song.title} img={song.img_src}/>);
       })}
     
     
--- main/src/Components/Card.jsx
+++ main/src/Components/Card.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-export default function Card({name,path, setMySound}) {
+export default function Card({name,path, setMySound,img}) {
     const handleClick=()=>{
         setMySound(path);
     }
   return (
-    <div className='w-[20rem] bg-[red] '>
-        <h1 className='w-full text-center font-bold text-[3rem]'>{name}</h1>
-        <button className='text-white w-full px-[2rem] py-[0.5rem]' onClick={handleClick}>Play</button>
+    <div className='w-[20rem] bg-[red] card  h-[25rem] relative' style={{backgroundImage:`url(${img})`}}>
+        
+        <button className='text-white w-full px-[2rem] py-[0.5rem] absolute bottom-[2rem]' onClick={handleClick}>Play</button>
     </div>
   )
 }
